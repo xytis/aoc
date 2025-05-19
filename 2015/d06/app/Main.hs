92cc7962a9b62063412e5ab24a955951e4b311b9
Vytis Valentinavičius <vytis@nolife4life.org>
2025-05-19 17:03:59 +0300

    2015: d06, mutable arrays

--- 2015/d06/app/Main.hs
+++ 2015/d06/app/Main.hs
@@ -1,44 +1,124 @@
+{-# LANGUAGE GADTs, StandaloneDeriving, TypeSynonymInstances, FlexibleInstances, DerivingStrategies #-}
+
 module Main where
 
 import Data.Array
-
-grid :: Ix t => (t, t) -> Array t Bool
-grid bnds = array bnds [(i, False) | i <- range bnds]
-
-limits :: ((Int, Int), (Int, Int))
---limits = ((0,0), (999,999))
-limits = ((0,0), (2,2))
-
-toggle :: Ix t => Array t Bool -> t -> Array t Bool
-toggle i a = i // [(a, not (i ! a))]
-
-on :: Ix t => Array t Bool -> t -> Array t Bool
-on i a = i // [(a, True)]
-
-off :: Ix t => Array t Bool -> t -> Array t Bool
-off i a = i // [(a, False)]
+import Data.Array.MArray
+import Data.Array.ST
+import qualified Text.Parsec as Parsec
+import Control.Monad.ST
+import Prelude hiding (flip)
 
 
--- Usage: mutate (grid limits) [(toggle, (range limits)), (toggle, (range ((1,1),(1,1)) )), (toggle, (range ((2,2),(2,2)) ))]
--- This function applies the listed operations in order on the grid, operating on full grid each time (that is the reason for
--- nested foldl)
-mutate :: Ix t => Array t Bool
-                    -> [(Array t Bool -> t -> Array t Bool, [t])]
-                    -> Array t Bool
-mutate = foldl apply
-  where
-    apply grd (mut, rng) = foldl mut grd rng
+example :: String
+example = "turn on 0,0 through 1,1\ntoggle 0,0 through 3,3\nturn off 2,2 through 3,3\n"
 
-lit :: Ix t => Array t Bool -> Int
-lit = length . (filter id) . elems
+attempt :: Parsec.Parsec String () a -> Either Parsec.ParseError a
+attempt rule = Parsec.parse rule "(example)" example
+
+data Action = On | Off | Toggle
+  deriving stock (Show, Eq)
+
+data Range = Range (Int, Int) (Int, Int)
+  deriving stock (Show, Eq)
+
+data Operation = Operation Action Range
+  deriving stock (Show, Eq)
+
+operationParser :: Parsec.Parsec String () Operation
+operationParser = do
+  let
+    on = On <$ Parsec.try (Parsec.string "turn on")
+    off = Off <$ Parsec.try (Parsec.string "turn off")
+    toggle = Toggle <$ Parsec.try (Parsec.string "toggle")
+    pair = do
+      x <- read <$> Parsec.many1 Parsec.digit
+      _ <- Parsec.char ','
+      y <- read <$> Parsec.many1 Parsec.digit
+      pure (x, y)
+
+  action <- Parsec.choice [on, off, toggle]
+  _ <- Parsec.space
+  (x1, y1) <- pair
+  _ <- Parsec.string " through "
+  (x2, y2) <- pair
+  pure $ Operation action $ Range (x1, y1) (x2, y2)
+
+inputParser :: Parsec.Parsec   String   ()   [Operation]
+inputParser = Parsec.endBy operationParser Parsec.newline
+
+
+-- >>> attempt inputParser
 
 part1 :: String -> Int
-part1 input = lit $ mutate (grid limits) [(toggle, (range limits)), (toggle, (range ((1,1),(1,1)) )), (toggle, (range ((2,2),(2,2)) ))]
+part1 input = 
+  let 
+    ops = case Parsec.parse inputParser "(part 1)" input of
+      Right o ->  o
+      Left e -> error (show e)
+    grid = runSTArray $ do
+      arr <- newArray ((0,0), (999,999)) False :: ST s (STGrid s Bool)
+      mapM_ (mutBool arr) ops 
+      return arr
+  in foldl' (\acc el -> if el then acc + 1 else acc) 0 grid
+
+part2 :: String -> Int
+part2 input =
+  let
+    ops = case Parsec.parse inputParser "(part 1)" input of
+      Right o ->  o
+      Left e -> error (show e)
+    grid = runSTArray $ do
+      arr <- newArray ((0,0), (999,999)) 0 :: ST s (STGrid s Int)
+      mapM_ (mutInt arr) ops 
+      return arr
+  in foldl' (+) 0 grid
+
+
+type Coords = (Int, Int)
+type Grid a = Array Coords a
+
+type STGrid s a = STArray s Coords a
+
+actBool :: Action -> STGrid s Bool -> Coords -> ST s ()
+actBool op arr at = do
+  case op of
+    On -> do
+      writeArray arr at True
+    Off -> do
+      writeArray arr at False
+    Toggle -> do
+      modifyArray arr at (not)
+
+actInt :: Action -> STGrid s Int -> Coords -> ST s ()
+actInt op arr at = do
+  case op of
+    On -> do
+      modifyArray arr at (+1)
+    Off -> do
+      modifyArray arr at (\v -> max (v-1) 0)
+    Toggle -> do
+      modifyArray arr at (+2)
+
+
+mutBool :: STGrid s Bool -> Operation -> ST s ()
+mutBool arr (Operation op (Range from to)) = do
+  mapM_ (actBool op arr) $ range (from, to)
+
+mutInt :: STGrid s Int -> Operation -> ST s ()
+mutInt arr (Operation op (Range from to)) = do
+  mapM_ (actInt op arr) $ range (from, to)
+
+test :: [Operation] -> Grid Bool
+test ops = runSTArray $ do
+  arr <- newArray ((0,0), (3,3)) False :: ST s (STGrid s Bool)
+  mapM_ (mutBool arr) ops 
+  return arr
 
 
 main :: IO ()
 main = do
   input <- readFile "input.txt"
   print $ "part 1: " ++ show (part1 input)
-  -- print $ "part 2: " ++ show (part2 input)
+  print $ "part 2: " ++ show (part2 input)
 
