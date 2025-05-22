727bbd306b29564eb842aa9cce8d58e9557f5349
Vytis Valentinavičius <vytis@nolife4life.org>
2025-05-22 06:25:58 +0300

    2015: d12, raw json rewrites

--- 2015/d12/app/Main.hs
+++ 2015/d12/app/Main.hs
@@ -15,7 +15,37 @@ part1 input =
       l = if (not $ null trail) then ((read $ reverse trail) :: Int) else 0
    in s + l
 
+data Ctx = Obj | Arr
+
+-- con drops current object tokens, direction based on first argument
+con :: Char -> [Ctx] -> [Char] -> [Char]
+con '}' [] ('}' : cs) = cs
+con '}' (Obj : xs) ('}' : cs) = con '}' xs cs
+con '}' (Arr : xs) (']' : cs) = con '}' xs cs
+con '}' xs ('{' : cs) = con '}' (Obj : xs) cs
+con '}' xs ('[' : cs) = con '}' (Arr : xs) cs
+con '{' [] ('{' : cs) = cs
+con '{' (Obj : xs) ('{' : cs) = con '{' xs cs
+con '{' (Arr : xs) ('[' : cs) = con '{' xs cs
+con '{' xs ('}' : cs) = con '{' (Obj : xs) cs
+con '{' xs (']' : cs) = con '{' (Arr : xs) cs
+con t xs (_ : cs) = con t xs cs
+con _ _ [] = []
+
+-- rednull replaces all objects with value "red" to null
+rednull :: [Char] -> [Char] -> [Char]
+rednull prev (':' : '"' : 'r' : 'e' : 'd' : '"' : next) =
+  let nprev = con '{' [] prev
+      nnext = con '}' [] next
+   in rednull nprev ("null" <> nnext)
+rednull prev (c : next) = rednull (c : prev) next
+rednull prev [] = reverse prev
+
+part2 :: String -> Int
+part2 input = part1 $ rednull [] input
+
 main :: IO ()
 main = do
   input <- readFile "input.txt"
   print $ part1 input
+  print $ part2 input
