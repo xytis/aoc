2140436e7ce6baa49ece4fb363ae236c76a0394c
Vytis Valentinavičius <vytis@nolife4life.org>
2025-12-09 13:40:27 +0200

    2025: d09, aabb method

--- 2025/d09/solve.py
+++ 2025/d09/solve.py
@@ -1,5 +1,5 @@
 import math
-from itertools import combinations
+from itertools import combinations, pairwise
 
 corners = [tuple(map(int, line.split(','))) for line in open('input.txt')]
 
@@ -14,66 +14,53 @@ areas = map(lambda p: area(*p), combinations(corners, 2))
 # part 1
 print(max(areas))
 
-from sympy.geometry import Point, Polygon, Segment, intersection
-
-# we need to revert Y axis, because the initial coords have been in screen system, and the rest of math works in paper :)
-#corners = list(map(lambda p: (p[0], -p[1]), corners))
+# part 2
+# construct all posible rectangles (and save in aabb form)
+# check if any of the rectangle lines (which are always axis aligned) cross any of the polygon lines.
+# that can be done if we imagine both rectangle and the polygon line as a aabb.
+# we tolerate touch, because if we have some funky indents, another polygon line will strike out the
+# rectangle.
 
 def expand(a, b):
     (x1, y1) = a
     (x2, y2) = b
-    # We need to ensure the rectangle is in clockwise direction
-    # There are only two orders (because of axis alignment), so we don't need fancy sort.
-    if x1 <= x2:
-        if y1 <= y2:
-            return Polygon(a, (x2, y1), b, (x1, y2))
-        else:
-            return Polygon(a, (x1, y2), b, (x2, y1))
-    else:
-        if y1 <= y2:
-            return Polygon(a, (x1, y2), b, (x2, y1))
-        else:
-            return Polygon(a, (x2, y1), b, (x1, y2))
+    # build a aabb from the rectangle
+    return ( (min(x1, x2), min(y1, y2)), (max(x1, x2), max(y1, y2)) )
 
-def is_on_border(point, polygon):
-    for segment in polygon.sides:
-        if point in segment:
-            return True
-    return False
+def crosses(aabb, line):
+    ((xmin, ymin), (xmax, ymax)) = aabb
+    ((x1, y1), (x2, y2)) = line
+    # convert line to "aabb"
+    umin = min(x1, x2)
+    umax = max(x1, x2)
+    vmin = min(y1, y2)
+    vmax = max(y1, y2)
 
-def inside(a, b):
-    if isinstance(b, Polygon):
-        #print("poly", b)
-        # check for line intesections
-        for side in b.sides:
-            #print("side", side)
-            # if any of the intersections are points, we have crossings or touching.
-            # all other segments are fine, because the next check will see if the segments end in correct locations.
-            for x in a.intersection(side):
-                if not isinstance(x, Segment):
-                    if not x in side.points:
-                        #print("nope: point", x)
-                        return False
-                    #print("skip: endpoint", x)
-        # check if all vertices are inside
-        for corner in b.vertices:
-            if not a.encloses_point(corner) and not is_on_border(corner, a):
-                #print("nope: outside", corner)
-                return False
-        return True
-    # We don't care for 1 width polygons, because they will not be in the answer anyway.
-    return False
+    # touch toleration as equality
+    arightb = umin >= xmax
+    aleftb = umax <= xmin
+    adownb = vmin >= ymax
+    aupb = vmax <= ymin
 
-def intarea(p):
-    [a, _, b, _] = p.vertices
-    return area(a, b)
+    return not (arightb or aleftb or adownb or aupb)
 
-tiled = Polygon(*corners)
 
-allrects = list(map(lambda p: expand(*p), combinations(corners, 2)))
+rectangles = list(map(lambda p: expand(*p), combinations(corners, 2)))
 
-#print("INNERS", allrects)
-#for i, r in enumerate(allrects):
-#    print(i, r)
+# close the polygon
+corners.append(corners[0])
 
-print(max(map(intarea, filter(lambda r: inside(tiled, r), allrects))))
+polylines = list(pairwise(corners))
+
+m = 0
+for r in rectangles:
+    ok = True
+    for l in polylines:
+        if crosses(r, l):
+            ok = False
+            break;
+    if ok:
+        #print("good", r, l)
+        m = max(area(*r), m)
+
+print(m)
